8319b488f5642cd889449c50940f31d374ee3fc5
Pooja2025-GI <dubeypooja70@gmail.com>
2025-12-07 13:12:42 +0530

    Assignments module_1

---
+++ Assignemnt2.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating the names
+full_name = first_name + " " + last_name
+
+# Printing a personalized greeting
+print(f"Hello, {full_name}! Welcome! to Python course Module 1")
